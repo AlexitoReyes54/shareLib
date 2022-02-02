8af97185e59dc17c1bdb8e81625052a2ca376b24
AlexReyes54 <49156377+AlexitoReyes54@users.noreply.github.com>
2022-02-02 11:31:26 -0400

    Update author.groovy

--- vars/author.groovy
+++ vars/author.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:] ){
   def json = libraryResource "org/heroku/bar.json"
-  def scriptTetx = libraryResource "org/scripts/hello.json"
+  def scriptTetx = libraryResource "org/scripts/hello.sh"
   echo json
   writeFile file:"hello", text:scriptTetx
   sh "chmod a+x ./hello"
