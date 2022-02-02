3e3850e7dd9d2dd9da2362470f79feafbce3e77c
AlexReyes54 <49156377+AlexitoReyes54@users.noreply.github.com>
2022-02-02 11:30:52 -0400

    Update author.groovy

--- vars/author.groovy
+++ vars/author.groovy
@@ -1,5 +1,8 @@
 def call(Map config = [:] ){
-  def request = libraryResource "org/heroku/bar.json"
-  echo request
+  def json = libraryResource "org/heroku/bar.json"
+  def scriptTetx = libraryResource "org/scripts/hello.json"
+  echo json
+  writeFile file:"hello", text:scriptTetx
+  sh "chmod a+x ./hello"
   sh "echo pipeline created by ${config.name}"
 }
